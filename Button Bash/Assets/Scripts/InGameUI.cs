5cf8058d24904d411992b581715af2ce717f94b7
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-10-07 14:48:56 +1100

    Added InGameUI.cs

---
+++ Button Bash/Assets/Scripts/InGameUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameUI : MonoBehaviour
+{
+	// The text box.
+	private Text m_Text;
+
+	void Awake()
+	{
+		// Get the text box the script is attatched to.
+		m_Text = GetComponent<Text>();
+	}
+
+	// Update the text on the text object.
+	// Params: the new text for the text box.
+	public void UpdateText(string newText)
+	{
+		m_Text.text = newText;
+	}
+}
