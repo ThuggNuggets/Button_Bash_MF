6a5675345864a922fb10bb544409df1a6bf6b2d5
sp0okyman <48226083+sp0okyman@users.noreply.github.com>
2019-10-08 09:54:29 +1100

    PlayerMovement work, needs testing

--- Button Bash/Assets/Scripts/PlayerMovement.cs
+++ Button Bash/Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,8 @@ public class PlayerMovement : MonoBehaviour
     public Transform railOne;
     // position of the second rail
     public Transform railTwo;
+    // gameState now exists and calls on existing GameManager script to get the current game state
+    GameManager.GameStates gameState = GameManager.GetInstance().GetGameState();
 
     // when movement started
     private float startTime;
@@ -28,6 +30,11 @@ public class PlayerMovement : MonoBehaviour
         rigidBody = GetComponent<Rigidbody>();
         // animator now has properties of Animator
         animator = GetComponent<Animator>();
+        // time when movement started
+        startTime = Time.time;
+        // calculate distance
+        distance = Vector3.Distance(railOne.position, railTwo.position);
+
     }
 
     void FixedUpdate()
@@ -37,14 +44,29 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+<<<<<<< .mine
+        switch (gameState)
+||||||| .r44
+        switch (GameManager.GetGameState())
+=======
         //switch (GameManager.GetGameState())
+>>>>>>> .r45
         {
             // execute on Playing state
            // case GameManager.GameStates.Playing:
                 Vector3 movement = Vector3.zero;
+                // distance moved = elapsed time * speed
+                float distanceCovered = (Time.time - startTime) * railSwitchSpeed;
+                // fraction of distance completed equals current distance divided by total distance
+                float fractionOfDistance = distanceCovered / distance;
 
+                // set position as a fraction of the distance between the rails
+                transform.position = Vector3.Lerp(railOne.position, railTwo.position, fractionOfDistance);
+
+                // horizontal movement along a rail
                 movement += transform.right * Input.GetAxis("Horizontal") * railMoveSpeed;
-                //movement += transform.forward * Input.GetAxis("Vertical") * railSwitchSpeed;
+
+                break;
         }
     }
 }
