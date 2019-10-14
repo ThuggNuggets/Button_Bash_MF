16a0da30e15f31489c97e209c63ad976dfb50849
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-10-14 14:57:26 +1100

    Added SelectCharacter.cs

---
+++ Button Bash/Assets/Scripts/SelectCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SelectCharacter : MonoBehaviour
+{
+	UnityEvent m_LockInCharacter;
+
+	private void Awake()
+	{
+		m_LockInCharacter = new UnityEvent();
+
+		m_LockInCharacter.AddListener(LockInCharacter);
+	}
+
+	public void LockInCharacter()
+	{
+		GameManager gm = GameManager.GetInstance();
+
+		
+	}
+}
