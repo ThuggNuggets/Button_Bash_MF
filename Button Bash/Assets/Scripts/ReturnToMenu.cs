3293655ec7e98672c552ab6e7b639d59f8d9706c
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-10-14 13:39:16 +1100

    Added ReturnToMenu and WinnerPortraitBehaviour

---
+++ Button Bash/Assets/Scripts/ReturnToMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class ReturnToMenu : MonoBehaviour
+{
+	// The return to menu event.
+	UnityEvent m_ReturnToMenuEvent;
+
+	// On startup.
+	private void Awake()
+	{
+		// Set the return to menu event to be a unity event.
+		m_ReturnToMenuEvent = new UnityEvent();
+
+		// Add the return to main menu function to the event.
+		m_ReturnToMenuEvent.AddListener(ReturnMainMenuMenu);
+	}
+
+	// Return the game to the main menu.
+	public void ReturnMainMenuMenu()
+	{
+		// Load the first scene (should be the main menu.)
+		SceneManager.LoadScene(0);
+	}
+}
---
+++ Button Bash/Assets/Scripts/WinnerPortraitBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinnerPortraitBehaviour : MonoBehaviour
+{
+	void Awake()
+	{
+		// Get the winning player.
+		int winningPlayer = GameObject.Find("game manager").GetComponent<GameManager>().GetWinningPlayer();
+
+		// Set the texture of the winning player's portrait to the portrait of the winning player.
+		GetComponent<RawImage>().texture = GetComponent<CharacterSelect>().m_PlayerPortraits[winningPlayer];
+	}
+}
