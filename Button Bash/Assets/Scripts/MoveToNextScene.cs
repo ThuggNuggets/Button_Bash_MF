27dd0004c817e0fe842c40c297d556f85ed494dc
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-10-15 11:57:06 +1100

    Can use a controller to move through the menus Replaced StartGame.cs with MoveToNextScene.cs, since it makes more sense now with multiple scenes and it actually works for moving to the next scene

---
+++ Button Bash/Assets/Scripts/MoveToNextScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using XboxCtrlrInput;
+
+public class MoveToNextScene : MonoBehaviour
+{
+	private void Update()
+	{
+		// If the A button is pressed, move on to the next scene.
+		if (XCI.GetButtonDown(XboxButton.A))
+		{
+			NextScene();
+		}
+	}
+
+	public void NextScene()
+	{
+		// Get the instance of the game manager.
+		GameManager gm = GameManager.GetInstance();
+
+		// Set the state of the game manager to playing.
+		gm.SetGameState(GameManager.GameStates.Playing);
+
+		// Load the next scene.
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
