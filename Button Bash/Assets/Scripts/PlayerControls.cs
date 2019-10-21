2e196dce7a1e5e95e608d30adf18022b0fbca2d6
sp0okyman <48226083+sp0okyman@users.noreply.github.com>
2019-10-21 14:35:26 +1100

    OnCollisionEnter changed to OnTriggerEnter

--- Button Bash/Assets/Scripts/PlayerControls.cs
+++ Button Bash/Assets/Scripts/PlayerControls.cs
@@ -136,7 +136,7 @@ public class PlayerControls : MonoBehaviour
 
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "ammopile")
         {
