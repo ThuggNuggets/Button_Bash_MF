eb2adb33797c8d4a2f2ac5a21c10d6656c3ed5e6
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-10-28 10:26:12 +1100

    Added DeadPlayerReticle.cs

---
+++ Button Bash/Assets/Scripts/DeadPlayerReticle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class DeadPlayerReticle : MonoBehaviour
+{
+	/// <summary>
+	/// The player number of the reticle.
+	/// </summary>
+	public int m_PlayerNumber;
+
+	/// <summary>
+	/// The x axis of the controller input.
+	/// </summary>
+	private float m_XAxis;
+
+	/// <summary>
+	/// The y axis of the controller input.
+	/// </summary>
+	private float m_YAxis;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+		m_XAxis = XCI.GetAxis(XboxAxis.LeftStickX, (XboxController)m_PlayerNumber + 1);
+		m_YAxis = XCI.GetAxis(XboxAxis.LeftStickY, (XboxController)m_PlayerNumber + 1);
+
+		Vector3 translation = new Vector3(m_XAxis, m_YAxis, 0);
+
+		translation *= Time.deltaTime;
+		transform.Translate(translation);
+    }
+}
