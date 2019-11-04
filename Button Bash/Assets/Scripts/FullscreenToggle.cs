22a4bff31c24d4eaa56da286e1b356128390ce35
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-11-04 12:23:53 +1100

    Added options screen with options, very basic and volume doesn't do anything and can't test fullscreen toggle.

---
+++ Button Bash/Assets/Scripts/FullscreenToggle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowedOnOff : MonoBehaviour
+{
+	/// <summary>
+	/// Toggle fullscreen.
+	/// </summary>
+	public void SetWindowed()
+	{
+		if (Screen.fullScreen == true)
+			Screen.fullScreen = false;
+		else
+			Screen.fullScreen = true;
+	}
+}
---
+++ Button Bash/Assets/Scripts/Windowed On Off.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FullscreenToggle: MonoBehaviour
+{
+	/// <summary>
+	/// On startup.
+	/// </summary>
+	private void Awake()
+	{
+		GetComponent<Toggle>().isOn = Screen.fullScreen;
+	}
+
+	/// <summary>
+	/// Toggle fullscreen.
+	/// </summary>
+	public void ToggleFullscreen()
+	{
+		if (Screen.fullScreen == true)
+			Screen.fullScreen = false;
+		else
+			Screen.fullScreen = true;
+
+		Debug.Log(Screen.fullScreen);
+	}
+}
