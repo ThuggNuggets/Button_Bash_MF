7be1573c383d7be835cb1f6a5bf90e1de852b8aa
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-11-12 13:19:19 +1100

    Added ReturnToCharacterSelect.cs, for convenience in going straight to the character select scene.

---
+++ Button Bash/Assets/Scripts/ReturnToCharacterSelect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using XboxCtrlrInput;
+
+public class ReturnToCharacterSelect : MonoBehaviour
+{
+	/// <summary>
+	/// If this script should check player input.
+	/// </summary>
+	public bool m_UseDirectPlayerInput = true;
+
+    /// <summary>
+	/// Update.
+	/// </summary>
+    void Update()
+    {
+		if (m_UseDirectPlayerInput == true)
+		{
+			if (XCI.GetButtonDown(XboxButton.A) == true)
+				GoToCharacterSelect();
+		}
+    }
+
+	/// <summary>
+	/// Load the character select scene.
+	/// </summary>
+	public void GoToCharacterSelect()
+	{
+		SceneManager.LoadScene(1);
+	}
+}
