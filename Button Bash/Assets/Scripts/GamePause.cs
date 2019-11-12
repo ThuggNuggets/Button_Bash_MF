991ab08cdb44c05590cb3b659186f32732c6818b
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-11-12 13:12:52 +1100

    More pause menu stuff, reset variables when exit to main menu.

--- Button Bash/Assets/Scripts/GamePause.cs
+++ Button Bash/Assets/Scripts/GamePause.cs
@@ -89,7 +89,6 @@ public class GamePause : MonoBehaviour
 			{
 				// Reset time and assign the player number to something not correlating to a controller.
 				Time.timeScale = 1.0f;
-				//m_PausedPlayerNumber = -1;
 				m_Paused = false;
 				m_PauseScreen.SetActive(false);
 			}
@@ -98,6 +97,8 @@ public class GamePause : MonoBehaviour
 			{
 				Debug.Log("Return to menu!");
 				Time.timeScale = 1.0f;
+				m_Paused = false;
+				m_PauseScreen.SetActive(false);
 				GetComponent<ReturnToMenu>().ReturnMainMenuMenu();
 			}
 		}
