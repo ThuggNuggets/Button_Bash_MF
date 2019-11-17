b4358d785e0b315eb130b218bd83d7a38b154f1d
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-11-18 09:08:48 +1100

    Added EndScreenReturnToMenu.cs, which has a delay to getting player input so the players don't skip past the end screen.

---
+++ Button Bash/Assets/Scripts/EndScreenReturnToMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using XboxCtrlrInput;
+
+public class EndScreenReturnToMenu : MonoBehaviour
+{
+	/// <summary>
+	/// The timer to delay input by.
+	/// </summary>
+	public float m_InputDelayTimer;
+
+	/// <summary>
+	/// Update.
+	/// </summary>
+	private void Update()
+	{
+		// Returns to menu when someone presses the A button after the delay timer has run out.
+		// So players don't skip past the end screen because the A button is used in the game and to leave the end screen.
+
+		if (m_InputDelayTimer <= 0.0f)
+		{
+			if (XCI.GetButton(XboxButton.A) == true)
+				SceneManager.LoadScene(0);
+		}
+		else
+			m_InputDelayTimer -= Time.deltaTime;
+	}
+}
