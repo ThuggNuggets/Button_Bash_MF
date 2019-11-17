a09be6b02efa856a446745155b05abe6041653e4
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-11-18 10:20:16 +1100

    Added ButtonPileReloadSound.cs, which will play a sound when a player collides with the button pile.

---
+++ Button Bash/Assets/Scripts/ButtonPileReloadSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPileReloadSound : MonoBehaviour
+{
+	/// <summary>
+	/// The audio source on the button pile.
+	/// </summary>
+	private AudioSource m_AudioSource;
+
+	private void Awake()
+	{
+		// Set the audio source's clip to the reload sound.
+		m_AudioSource = GetComponent<AudioSource>();
+	}
+
+	/// <summary>
+	/// When something collides with the button pile.
+	/// </summary>
+	/// <param name="collision"></param>
+	private void OnCollisionEnter(Collision collision)
+	{
+		// Play the sound in the audio source, should be the reload sound.
+		m_AudioSource.Play();
+	}
+}
