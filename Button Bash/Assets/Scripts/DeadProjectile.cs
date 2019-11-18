3c1e13bd6c25ce2aa6abf3eee583c9d45f165ec0
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-11-18 11:35:26 +1100

    Dead player projectiles will bounce when they hit something, continuing forward rather than straight up.

--- Button Bash/Assets/Scripts/DeadProjectile.cs
+++ Button Bash/Assets/Scripts/DeadProjectile.cs
@@ -20,22 +20,6 @@ public class DeadProjectile : MonoBehaviour
 	public float m_DespawnTimer;
 
 	/// <summary>
-	/// If the dead player projectile has collided with something yet.
-	/// </summary>
-	private bool m_Collided = false;
-
-	/// <summary>
-	/// The speed at which the button is shot up into the air.
-	/// </summary>
-	public float m_FlingSpeed;
-
-	private int m_RotationIterator = 0;
-
-	public float m_XFling;
-
-	public float m_ZFling;
-
-	/// <summary>
 	/// On startup.
 	/// </summary>
 	private void Awake()
@@ -43,9 +27,6 @@ public class DeadProjectile : MonoBehaviour
 		// Get the rigidbody of the projectile.
 		// For moving the projectile.
 		m_Rigidbody = GetComponent<Rigidbody>();
-
-		m_XFling = Random.Range(-m_XFling, m_XFling);
-		m_ZFling = Random.Range(-m_ZFling, m_ZFling);
 	}
 
 	/// <summary>
@@ -60,33 +41,9 @@ public class DeadProjectile : MonoBehaviour
 		else
 		{
 			m_DespawnTimer -= Time.deltaTime;
-			// If the button has not collided with something, move the button foward.
-			// Else, fling the button up into the air and rotate around.
-			if (m_Collided == false)
-				m_Rigidbody.MovePosition(transform.position + ((-transform.right) * m_Force * Time.deltaTime));
-			else
-			{
-				transform.Translate(new Vector3(m_XFling, m_FlingSpeed, m_ZFling) * Time.deltaTime, Space.World);
 
-				// Rotate the button, so it isn't boring.
-				switch(m_RotationIterator)
-				{
-					case 0:
-						transform.Rotate(10, 0, 0);
-						m_RotationIterator = 1;
-						break;
-
-					case 1:
-						transform.Rotate(0, 10, 0);
-						m_RotationIterator = 2;
-						break;
-
-					case 2:
-						transform.Rotate(0, 0, 10);
-						m_RotationIterator = 0;
-						break;
-				}
-			}
+			// Move the projectile foward.
+			m_Rigidbody.MovePosition(transform.position + ((-transform.right) * m_Force * Time.deltaTime));
 		}
     }
 
@@ -96,8 +53,6 @@ public class DeadProjectile : MonoBehaviour
 	/// <param name="collision">What the button collided with.</param>
 	private void OnCollisionEnter(Collision collision)
 	{
-		// We want the button to fly up into the air when it collides with something.
-
-		m_Collided = true;
+		transform.Rotate(new Vector3(0,0, -45));
 	}
 }
