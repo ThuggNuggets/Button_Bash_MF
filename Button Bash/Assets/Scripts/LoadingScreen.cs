b03a508280b220a26113e38481313d1ab02a3c40
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-11-18 16:29:25 +1100

    Added script for the fake loading screen, for showing instructions, the canvas is blank.

---
+++ Button Bash/Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+	/// <summary>
+	/// The timer for the fake loading screen to stay up for.
+	/// </summary>
+	public float m_LoadTime;
+
+	/// <summary>
+	/// Update.
+	/// </summary>
+	private void Update()
+	{
+		// Decrement the time, when it reaches 0, load the game.
+		if (m_LoadTime <= 0.0f)
+			SceneManager.LoadScene(3);
+		else
+			m_LoadTime -= Time.deltaTime;
+	}
+}
