734d050566c4fbe70b716b37979d8e4c2ff6dc0d
eclipse479 <49893045+eclipse479@users.noreply.github.com>
2019-11-25 12:27:04 +1100

    updated buttons + prefab

--- Button Bash/Assets/Scripts/PlayerProjectile.cs
+++ Button Bash/Assets/Scripts/PlayerProjectile.cs
@@ -34,10 +34,11 @@ public class PlayerProjectile : MonoBehaviour
 	void FixedUpdate()
     {
 		// Move the projectile by it's position + the foward direction (foward for the game's orientation, it's actually left) * the force on the projectile.
-		m_Rigidbody.MovePosition(transform.position + (-transform.right) * m_Force);
+		//m_Rigidbody.MovePosition(transform.position - transform.right * m_Force);
+        transform.Translate(new Vector3(-m_Force, 0, 0) * Time.deltaTime, Space.World);
 
-		// If the timer is equal to 0, destroy the projectile.
-		if (m_Timer <= 0.0f)
+        // If the timer is equal to 0, destroy the projectile.
+        if (m_Timer <= 0.0f)
 			Destroy(gameObject);
 
 		// Else, decrease the projectile by delta time.
