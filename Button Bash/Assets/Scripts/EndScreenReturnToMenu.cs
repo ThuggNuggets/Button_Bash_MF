8e41ff2a54ecf4ec6dfb79c8eb3d87f7dfaea65c
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-11-26 15:15:16 +1100

    Vibration on the last controller is reset at the end of the round.

--- Button Bash/Assets/Scripts/EndScreenReturnToMenu.cs
+++ Button Bash/Assets/Scripts/EndScreenReturnToMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using XboxCtrlrInput;
+using XInputDotNetPure;
 
 public class EndScreenReturnToMenu : MonoBehaviour
 {
@@ -10,6 +11,15 @@ public class EndScreenReturnToMenu : MonoBehaviour
 	/// The timer to delay input by.
 	/// </summary>
 	public float m_InputDelayTimer;
+
+	private void Awake()
+	{
+		// Set the vibration on all the controllers to 0, for the one person still vibrating at the end of the round.
+		GamePad.SetVibration(PlayerIndex.One, 0.0f, 0.0f);
+		GamePad.SetVibration(PlayerIndex.Two, 0.0f, 0.0f);
+		GamePad.SetVibration(PlayerIndex.Three, 0.0f, 0.0f);
+		GamePad.SetVibration(PlayerIndex.Four, 0.0f, 0.0f);
+	}
 
 	/// <summary>
 	/// Update.
